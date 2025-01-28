ecdd9ee5573709dadb19b6d12e712c61c2bc0938
Ankit Khanal <ankitkhanal231@gmail.com>
2025-01-28 11:47:43 +0545

    feat: hackpy to create file in hackerrank folder in utilities.py

--- utilties.py
+++ utilties.py
@@ -1,16 +1,25 @@
 from slugify import slugify
 import sys
+import os
 
-available_operations = ["slugify", "slugpy"]
+available_operations = ["slugify", "slugpy", "hackpy"]
 
 
 def get_slug(text):
     return slugify(text, separator="_")
 
 
+def create_file(directory: str, filename: str):
+    full_path = os.path.join(directory, filename)
+    f = open(full_path, "w")
+    f.close()
+
+
 def print_help():
     print("Usage: python utilities.py --<operation> <text>")
     print("Usage slugify: python utilities.py --slugify <text>")
+    print("Usage slugpy: python utilities.py --slugpy <text>")
+    print("Usage hackpy: python utilities.py --hackpy <text>")
     print(f"Available Operations: {available_operations}")
 
 
@@ -30,9 +39,18 @@ if __name__ == "__main__":
             if actual_operation not in available_operations:
                 print_help()
 
-            if actual_operation == "slugify" or actual_operation == "slugpy":
+            if (
+                actual_operation == "slugify"
+                or actual_operation == "slugpy"
+                or actual_operation == "hackpy"
+            ):
                 text = " ".join(sys.argv[2:])
                 slug = get_slug(text)
                 if actual_operation == "slugpy":
                     slug = slug + ".py"
+
+                if actual_operation == "hackpy":
+                    directory = "hackerrank"
+                    create_file(directory, slug)
+                    print(f"Created {slug} in {directory}.")
                 print(slug)
