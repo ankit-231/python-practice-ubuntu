deea17f1bc64557ef972fe641bde2d0e07dbaea8
Ankit Khanal <ankitkhanal231@gmail.com>
2026-02-25 21:00:05 +0545

    feat: german audio downloader

---
+++ general/download_german_audio.py
@@ -0,0 +1,111 @@
+import os
+
+import requests
+
+BASE_FILE_DIR = "files/german-audio"
+
+
+def download_audio(url: str, filename: str):
+    try:
+        response = requests.get(url, stream=True)
+        response.raise_for_status()  # Raise error for bad status codes
+        os.makedirs(BASE_FILE_DIR, exist_ok=True)
+        with open(filename, "wb") as f:
+            for chunk in response.iter_content(chunk_size=8192):
+                if chunk:
+                    f.write(chunk)
+
+        print(f"Download complete: {filename}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Download failed: {e}")
+
+
+MUSIC_FILE_EXTENSIONS = [
+    # Lossy
+    "mp3",
+    "aac",
+    "m4a",
+    "ogg",
+    "opus",
+    "wma",
+    "mpc",
+    "amr",
+    "ra",
+    "rm",
+    "ac3",
+    "dts",
+    # Lossless
+    "wav",
+    "aiff",
+    "aif",
+    "flac",
+    "alac",
+    "ape",
+    "wv",
+    "tta",
+    "tak",
+    "m4a",
+    # Uncompressed
+    "pcm",
+    "bwf",
+    # MIDI
+    "mid",
+    "midi",
+    "kar",
+    "rmi",
+    # Tracker / Module
+    "mod",
+    "xm",
+    "it",
+    "s3m",
+    "mtm",
+    "umx",
+    # Playlists
+    "m3u",
+    "m3u8",
+    "pls",
+    "wpl",
+    "xspf",
+    # Containers
+    "mp4",
+    "mkv",
+    "avi",
+    "mov",
+    "webm",
+    # Game / Specialized
+    "vgm",
+    "nsf",
+    "spc",
+    "psf",
+    "adx",
+    # Other
+    "caf",
+    "au",
+    "snd",
+    "8svx",
+    "voc",
+    "qcp",
+    "gsm",
+]
+
+
+def guess_filetype_from_url(url: str):
+    extension = url.split(".")[-1]
+    print("ogg" in MUSIC_FILE_EXTENSIONS)
+    if extension.lower() in MUSIC_FILE_EXTENSIONS:
+        return extension.lower()
+    return None
+
+
+def generate_full_path_with_extension(filename: str, url: str):
+    extension = guess_filetype_from_url(url)
+    if extension is None:
+        raise ValueError("Extension could not be guessed")
+    return f"{BASE_FILE_DIR}/{filename}.{extension}"
+
+
+if __name__ == "__main__":
+    url = "https://dict.leo.org/media/audio/B7NBkcBfiRlJ5y1o4uC-og.ogg"
+    full_path = generate_full_path_with_extension("trinken", url)
+    download_audio(url, full_path)
