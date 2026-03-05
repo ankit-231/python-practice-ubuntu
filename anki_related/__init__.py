4691cf0327f0440581fdf740c85b79912d7ee197
Ankit Khanal <ankitkhanal231@gmail.com>
2026-03-05 21:17:09 +0545

    docs: add usage instructions for downloading German audio files in __init__.py

---
+++ anki_related/__init__.py
@@ -0,0 +1,5 @@
+"""
+You have two options to download the audio files for German words:
+1. Use the `download_german_audio.py` script to download audio files manually and add it to anki.
+2. Automatically search word on leo.org from terminal and add it to anki using `auto_add_from_leo.py` script. This will also download the audio and add it to anki.
+"""
