b21a759dc586bd24d398ad73c5dd6fa6ace73fb5
Ankit Khanal <ankitkhanal231@gmail.com>
2026-06-30 11:19:05 +0545

    temp: random things related to lists

--- temp.py
+++ temp.py
@@ -6,5 +6,44 @@ def temp():
     # local_time =
 
 
+def remove_all_occurrences(lst: list, value: object) -> list:
+    """
+    removes in place from the list all occurrences of the value
+    """
+    while value in lst:
+        lst.remove(value)
+
+
+def shares():
+    all_shares = [
+        "SYPNL",
+        "SAGAR",
+        "SYPNL",
+        "BANDIPUR",
+        "DHEL",
+        "BANDIPUR",
+        "SAGAR",
+        "DHEL",
+        "RNLI",
+        "BHDC",
+        "KBLPO",
+        "KBL",
+        "RNLI",
+        "HRL",
+        "VLUCL",
+        "RNLI",
+        "BHDC",
+        "MBJC",
+    ]
+
+    remove_all_occurrences(all_shares, "KBLPO")
+    remove_all_occurrences(all_shares, "KBL")
+
+    unique_shares = list(set(all_shares))
+    print(unique_shares)
+    print(len(unique_shares))
+    # ['RNLI', 'DHEL', 'BHDC', 'BANDIPUR', 'SYPNL', 'SAGAR', 'VLUCL', 'MBJC', 'HRL']
+
+
 if __name__ == "__main__":
-    temp()
+    shares()
